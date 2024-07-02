a5ba5bad9ccb6bfdce2c1d330cd0f17e644de5f5
vigneshkandiah <vigneshkandiah@gmail.com>
2024-07-02 21:12:56 +0530

    321456-RHEL9 CreateParty Regression positive

---
+++ TestAutomation/src/main/java/javaPrograms/LearnOddNumbers.java
@@ -0,0 +1,19 @@
+package javaPrograms;
+
+public class LearnOddNumbers {
+	
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			
+			if(i%2!=0) {
+				
+				System.out.println(i);
+			}
+			
+		}
+		
+	}
+
+}
